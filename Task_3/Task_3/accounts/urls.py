a4c2c67f1a49d02a772cee99eb01e68c9a14eb5a
meitiii <mahdifekri1384414@gmail.com>
2025-12-04 10:32:19 +0330

    docs(task-5): add task 5 documentation

--- Task_3/Task_3/accounts/urls.py
+++ Task_3/Task_3/accounts/urls.py
@@ -4,5 +4,6 @@ from .views import RegisterUser ,EditProfileModel , Profile_Details
 urlpatterns = [
 path('register/',RegisterUser.as_view(),name = 'register'),
 path('ProfileEdit/',EditProfileModel.as_view(),name = 'edit_profile'),
-path('mypage',Profile_Details.as_view(),name='mypage')
+path('mypage',Profile_Details.as_view(),name='mypage'),
+path('')
 ]   
