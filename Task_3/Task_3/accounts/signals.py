8a9263e92e5c721da8cbc98b2e5374d08a2d8106
meitiii <mahdifekri1384414@gmail.com>
2025-12-21 23:35:55 +0330

    feat(task-5):add post-save to account app for create profile automatically

--- Task_3/Task_3/accounts/signals.py
+++ Task_3/Task_3/accounts/signals.py
@@ -4,13 +4,20 @@ from .models import Profile
 from django.dispatch import receiver
 from django.contrib.auth.models import User,Group
 
-def User_post_save(sender,instance,*args, **kwargs):
+def User_post_save(sender,instance,created,*args, **kwargs):
     print('post_save signal called')
-    print(sender,instance)
-post_save.connect(User_post_save,sender=Profile)
+    # print(sender,instance)
+    if created:
+        Profile.objects.get_or_create(user =instance)
+        developer_group = Group.objects.filter(name='Developer').first()        
+        instance.groups.add(developer_group)
 
 
-def User_pre_save(sender,instance,*args, **kwargs):
-     print('pre_save signal called')
-     print(sender,instance)
-pre_save.connect(User_pre_save,sender=Profile)
+post_save.connect(User_post_save,sender=User)
+
+
+
+# def User_pre_save(sender,instance,*args, **kwargs):
+#      print('pre_save signal called')
+#      print(sender,instance)
+# pre_save.connect(User_pre_save,sender=Profile)
